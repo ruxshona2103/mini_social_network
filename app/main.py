9a1ba545bdd770a40573d4b9b714b8956f0034bd
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-03-14 08:55:36 +0500

    main.py updated new routers added

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
+
 from app.core.config import settings
+from app.api.endpoints import user, auth, posts, comment, like
+import app.models
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
     description=settings.PROJECT_DESCRIPTION
 )
+
+app.include_router(auth.router, tags=["Login"])
+app.include_router(user.router, prefix="/users", tags=["Users"])
+app.include_router(posts.router, prefix="/posts", tags=["Posts"])
+app.include_router(comment.router, tags=["Comments"])
+app.include_router(like.router, tags=["Likes"])
 
 @app.get("/")
 async def root():
